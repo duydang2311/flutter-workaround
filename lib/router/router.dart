93eaf81fac72e86b241662fcd6f9399b37c93382
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-03-07 20:51:51 +0700

    lint: remove unused imports

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,8 +1,5 @@
-import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:workaround/authentication/bloc/authentication_bloc.dart';
 import 'package:workaround/sign_in/sign_in.dart';
 import 'package:workaround/splash/splash.dart';
 
