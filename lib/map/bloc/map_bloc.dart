8a29e70472bd9985852778cdb185b461e977cb0c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2024-05-10 13:12:56 +0700

    feat(map): add address and update map on location changed

--- lib/map/bloc/map_bloc.dart
+++ lib/map/bloc/map_bloc.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
 import 'package:location_client/location_client.dart';
 import 'package:shared_kernel/shared_kernel.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:work_repository/work_repository.dart';
+import 'package:workaround/create_work/create_work.dart';
+import 'package:workaround/dart_define.gen.dart';
 import 'package:workaround/map/map.dart';
 
 part 'map_event.dart';
@@ -17,22 +22,32 @@ class MapBloc extends Bloc<MapEvent, MapState> {
   MapBloc({
     required WorkRepository workRepository,
     required LocationClient locationClient,
+    required Client client,
   })  : _workRepository = workRepository,
         _locationClient = locationClient,
+        _client = client,
         super(const MapState()) {
     on<MapInitialized>(_handleMapInitialized);
     on<MapChanged>(_handleMapChanged);
     on<MapWorkTapped>(_handleWorkTapped);
-    _streamSubscription = _workRepository.stream.listen(_handleWorkChanged);
+    on<_MapStatusChanged>(_handleStatusChanged);
+    on<_MapWorksChanged>(_handleWorksChanged);
+    on<_MapAddressChanged>(_handleAddressChanged);
   }
 
   final WorkRepository _workRepository;
   final LocationClient _locationClient;
+  final Client _client;
   late final StreamSubscription<PostgresChangePayload> _streamSubscription;
+  late final StreamSubscription<Position>? _positionStreamSubscription;
+  Position? _lastPosition;
+  double _distance = 0;
+  static const double _distanceThreshold = 100;
 
   @override
   Future<void> close() async {
     await _streamSubscription.cancel();
+    await _positionStreamSubscription?.cancel();
     return super.close();
   }
 
@@ -40,18 +55,73 @@ class MapBloc extends Bloc<MapEvent, MapState> {
     MapInitialized event,
     Emitter<MapState> emit,
   ) async {
-    emit(
-      state.copyWith(
-        status: MapStatus.pending,
-        positionStream: _locationClient.getPositionStream(
-          const LocationSettings(),
+    _streamSubscription = _workRepository.stream.listen(_handleWorkChanged);
+
+    emit(state.copyWith(status: MapStatus.pending));
+    await _locationClient
+        .checkPermission()
+        .toTaskEither<GenericError>()
+        .flatMap<LocationPermission>(
+          (r) => switch (r) {
+            LocationPermission.denied => _locationClient.requestPermission(),
+            LocationPermission.always ||
+            LocationPermission.whileInUse =>
+              TaskEither.right(r),
+            _ => TaskEither.left(const GenericError.unknown()),
+          },
+        )
+        .flatMap<LocationPermission>(
+          (r) => switch (r) {
+            LocationPermission.always ||
+            LocationPermission.whileInUse =>
+              TaskEither.right(r),
+            _ => TaskEither.left(const GenericError.unknown()),
+          },
+        )
+        .match((l) {
+      emit(
+        state.copyWith(
+          error: Option.of(
+            UiError.now(
+              message: l.formatted,
+            ),
+          ),
         ),
-      ),
-    );
+      );
+    }, (r) {
+      final positionStream = _locationClient.getPositionStream(
+        settings: const LocationSettings(
+          accuracy: LocationAccuracy.bestForNavigation,
+        ),
+      );
+      _positionStreamSubscription = positionStream.listen(
+        _handleCurrentPosition,
+        onError: (dynamic error, stackTrace) {
+          emit(
+            state.copyWith(
+              error: Option.of(
+                UiError.now(
+                  message: switch (error) {
+                    final Exception e => GenericError.fromException(e),
+                    _ => const GenericError.unknown(),
+                  }
+                      .formatted,
+                ),
+              ),
+            ),
+          );
+        },
+      );
+      emit(state.copyWith(positionStream: positionStream));
+    }).run();
+
     final mapWorks = await _nearbyMapWorksTask.run();
     emit(
       state.copyWith(
-        mapWorks: {for (final e in mapWorks) e.id: e},
+        mapWorks: {
+          for (final e in mapWorks)
+            e.id: e.copyWith(description: state.mapWorks[e.id]?.description),
+        },
         status: MapStatus.none,
       ),
     );
@@ -65,7 +135,10 @@ class MapBloc extends Bloc<MapEvent, MapState> {
     final mapWorks = await _nearbyMapWorksTask.run();
     emit(
       state.copyWith(
-        mapWorks: {for (final e in mapWorks) e.id: e},
+        mapWorks: {
+          for (final e in mapWorks)
+            e.id: e.copyWith(description: state.mapWorks[e.id]?.description),
+        },
         status: MapStatus.none,
       ),
     );
@@ -108,12 +181,97 @@ class MapBloc extends Bloc<MapEvent, MapState> {
               event.id: work.copyWith(
                 description: Option.fromNullable(r.description),
                 popupStatus: PopupStatus.none,
-              )
+              ),
           },
           error: const Option.none(),
         ),
       );
     }).run();
+  }
+
+  void _handleStatusChanged(_MapStatusChanged event, Emitter<MapState> emit) {
+    emit(state.copyWith(status: event.status));
+  }
+
+  void _handleWorksChanged(_MapWorksChanged event, Emitter<MapState> emit) {
+    emit(state.copyWith(mapWorks: event.mapWorks));
+  }
+
+  void _handleAddressChanged(_MapAddressChanged event, Emitter<MapState> emit) {
+    emit(state.copyWith(address: event.address));
+  }
+
+  Future<void> _handleCurrentPosition(Position position) async {
+    if (_lastPosition != null) {
+      _distance += Geolocator.distanceBetween(
+        _lastPosition!.latitude,
+        _lastPosition!.longitude,
+        position.latitude,
+        position.longitude,
+      );
+      if (_distance < _distanceThreshold) return;
+      _distance -= _distanceThreshold;
+    }
+    _lastPosition = position;
+    add(const _MapStatusChanged(status: MapStatus.pending));
+    final (works, address) = await (
+      _workRepository
+          .getNearbyWorks(position.latitude, position.longitude)
+          .map(
+            (r) => List<MapWork>.from(
+              r.map(
+                (e) => MapWork(
+                  id: e.id,
+                  title: e.title,
+                  lat: e.lat,
+                  lng: e.lng,
+                  distance: e.distance,
+                ),
+              ),
+            ),
+          )
+          .getOrElse((l) => const [])
+          .run(),
+      TaskEither.tryCatch(
+        () => _client.get(
+          Uri.https(
+            'rsapi.goong.io',
+            '/Geocode',
+            {
+              'latlng': '${position.latitude}, ${position.longitude}',
+              'api_key': DartDefine.goongApiKey,
+            },
+          ),
+        ),
+        (error, _) => switch (error) {
+          final Exception e => GenericError.fromException(e),
+          _ => const GenericError.unknown(),
+        },
+      )
+          .map(
+            (response) => jsonDecode(utf8.decode(response.bodyBytes))
+                as Map<String, dynamic>,
+          )
+          .flatMap(
+            (json) => TaskEither.fromNullable(
+              (json['results'] as List?)?.first as Map<String, dynamic>?,
+              () => const GenericError(
+                message: 'Could not convert JSON.',
+              ),
+            ),
+          )
+          .map(Place.fromJson)
+          .match((l) => 'Unknown location', (r) => r.address)
+          .run(),
+    ).wait;
+    add(_MapWorksChanged(
+      mapWorks: {
+        for (final e in works)
+          e.id: e.copyWith(description: state.mapWorks[e.id]?.description),
+      },
+    ));
+    add(_MapAddressChanged(address: address));
+    add(const _MapStatusChanged(status: MapStatus.none));
   }
 
   Task<List<MapWork>> get _nearbyMapWorksTask => _locationClient
@@ -152,4 +310,70 @@ class MapBloc extends Bloc<MapEvent, MapState> {
         ),
       )
       .getOrElse((l) => const []);
+
+  TaskEither<GenericError, Stream<Position>> _getPositionStreamWithRetry({
+    LocationSettings? settings,
+    bool retry = true,
+  }) =>
+      TaskEither.tryCatch(
+        () async {
+          // log('${await _locationClient.checkPermission().run()}');
+          final stream = _locationClient.getPositionStream(settings: settings);
+          await stream.listen((data) {
+            log('_data: $data');
+          }).cancel();
+          return _locationClient.getPositionStream(settings: settings);
+        },
+        (error, _) => switch (error) {
+          final TimeoutException e =>
+            GenericError.fromException(e, code: 'timeout'),
+          final LocationServiceDisabledException e =>
+            GenericError.fromException(e, code: 'disabled'),
+          final Exception e => GenericError.fromException(e),
+          _ => const GenericError.unknown(),
+        },
+      ).orElse(
+        (l) {
+          log('_orElse $l');
+          return switch (retry) {
+            true => _toTaskEitherStreamPosition(l, settings: settings),
+            false => TaskEither.left(l),
+          };
+        },
+      );
+
+  TaskEither<GenericError, Stream<Position>> _toTaskEitherStreamPosition(
+    GenericError error, {
+    LocationSettings? settings,
+  }) {
+    return switch (error.code) {
+      'disabled' =>
+        _locationClient.checkPermission().toTaskEither<GenericError>(),
+      _ => TaskEither<GenericError, LocationPermission>.left(
+          const GenericError.unknown(),
+        ),
+    }
+        .flatMap(
+          (r) {
+            log('_permission: $r');
+            return switch (r) {
+              LocationPermission.denied => _locationClient.requestPermission(),
+              _ => TaskEither.right(r),
+            };
+          },
+        )
+        .flatMap(
+          (r) => switch (r) {
+            LocationPermission.always ||
+            LocationPermission.whileInUse =>
+              TaskEither.right(r),
+            _ => TaskEither<GenericError, LocationPermission>.left(
+                const GenericError.unknown(),
+              ),
+          },
+        )
+        .flatMap(
+          (r) => _getPositionStreamWithRetry(settings: settings, retry: false),
+        );
+  }
 }
--- lib/map/bloc/map_state.dart
+++ lib/map/bloc/map_state.dart
@@ -6,26 +6,36 @@ final class MapState extends Equatable {
     this.error = const Option.none(),
     this.positionStream = const Stream.empty(),
     this.status = MapStatus.none,
+    this.address = '',
   });
 
   final Map<String, MapWork> mapWorks;
   final Option<UiError> error;
   final Stream<Position> positionStream;
   final MapStatus status;
+  final String address;
 
   MapState copyWith({
     Map<String, MapWork>? mapWorks,
     Option<UiError>? error,
     Stream<Position>? positionStream,
     MapStatus? status,
+    String? address,
   }) =>
       MapState(
         mapWorks: mapWorks ?? this.mapWorks,
         error: error ?? this.error,
         positionStream: positionStream ?? this.positionStream,
         status: status ?? this.status,
+        address: address ?? this.address,
       );
 
   @override
-  List<Object?> get props => [mapWorks, error, positionStream, status];
+  List<Object?> get props => [
+        mapWorks,
+        error,
+        positionStream,
+        status,
+        address,
+      ];
 }
